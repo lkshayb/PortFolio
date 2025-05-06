1127939a9cc990782a7a8880cad6f3e6e222ef7e
lakshay bhatia <bhatia00lakshay@gmail.com>
2025-05-06 22:58:29 +0530

    About Section Update

--- src/comp/About.jsx
+++ src/comp/About.jsx
@@ -1,8 +1,22 @@
 export default function About(){
     return(
-        <div className="h-[70vh] bg-">
-            <div>
-                <span className="text-4xl font-sans font-bold my-[25px]">About Me</span>
+        <div className="h-[90vh] flex-col">
+            <div className="text-white mb-20">.</div>
+
+            <div className="flex items-center justify-center ">
+                <div className="text-center max-w-2xl">
+                    <div className="mb-5">
+                        <span className="text-4xl font-sans font-bold">About Me</span>    
+                    </div>
+                    <div className="flex justify-center mb-13">
+                        <hr className="w-24 h-1 bg-violet-400 border-0" />
+                    </div>
+                    <p className="text-lg font-gray-600">I'm Lakshay Bhatia, an aspiring developer with a strong interest in AI, machine learning and web technologies. I'm passionate about creating elegant solutions to complex problems and continuously learning new technologies.</p>
+                </div>
+            </div>
+
+            <div className="rounded-lg shadow-lg hover:shadow-2xl duration-200 ease-in w-50 p-10">
+                Education
             </div>
         </div>
     )
