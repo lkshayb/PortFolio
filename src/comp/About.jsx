22206cad1938680f4b0f6b2862714b72fad8ce75
lakshay bhatia <bhatia00lakshay@gmail.com>
2025-05-07 12:08:45 +0530

    Projects Section Updated

--- src/comp/About.jsx
+++ src/comp/About.jsx
@@ -1,6 +1,6 @@
 export default function About(){
     return(
-        <div className="h-[90vh] flex-col">
+        <div className="h-[90vh] flex-col mb-20">
             <div className="text-white mb-20">.</div>
 
             <div className="flex items-center justify-center ">
@@ -8,7 +8,7 @@ export default function About(){
                     <div className="mb-5">
                         <span className="text-4xl font-sans font-bold">About Me</span>    
                     </div>
-                    <div className="flex justify-center mb-13">
+                    <div className="flex justify-center mb-11">
                         <hr className="w-24 h-1 bg-violet-400 border-0" />
                     </div>
                     <p className="text-lg font-gray-600">I'm Lakshay Bhatia, an aspiring developer with a strong interest in AI, machine learning and web technologies. I'm passionate about creating elegant solutions to complex problems and continuously learning new technologies.</p>
--- src/comp/Project.jsx
+++ src/comp/Project.jsx
@@ -1,9 +1,35 @@
+import { div } from "three/tsl"
+
 export default function Project(){
-    return(
-        <div className="h-[90vh] bg-">
-            <div>
-                <span className="text-4xl font-sans font-bold my-[25px]">Projects</span>
+    function ProdCard(props){
+        return(
+            <div className="bg-white pb-10 rounded-xl w-100 shadow-md hover:shadow-xl duration-200 ease-in">
+                <img src={props.image} className="rounded-t-xl hover:scale-102 duration-200 ease-in hover:rounded-xl" alt="" />
+                <span>{props.ProjectName}</span>
             </div>
+        )
+    }
+    return(
+        <div className="h-[90vh] bg-violet-100 mb-20">
+            <div className="text-white mb-20">.</div>
+            <div className="flex items-center justify-center ">
+                <div className="text-center max-w-2xl">
+                    <div className="mb-5">
+                        <span className="text-4xl font-sans font-bold">My Projects</span>    
+                    </div>
+                    <div className="flex justify-center mb-11">
+                        <hr className="w-24 h-1 bg-violet-400 border-0" />
+                    </div>
+                    <p className="text-lg font-gray-600">Here are some of my recent projects that showcase my technical skills and problem-solving abilities.</p>
+                </div>
+                
+            </div> 
+            <div className="flex gap-5 justify-center mt-10">
+                <ProdCard ProjectName="SlangSter" image="./slangster.png"/>
+                <ProdCard ProjectName="IPScout" image="./ipscout.png"/>
+                <ProdCard ProjectName="TalkSpace" image="./talkspace.png"/>
+            </div>
+            
         </div>
     )
 }
