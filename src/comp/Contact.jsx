ff18c907593d29740f133515ead35cf515b27f92
lakshay bhatia <bhatia00lakshay@gmail.com>
2025-05-07 15:47:46 +0530

    Skills section completed

--- src/comp/Contact.jsx
+++ src/comp/Contact.jsx
@@ -1,9 +1,23 @@
 export default function Contact(){
     return(
-        <div className="h-[70vh] bg-">
-            <div>
-                <span className="text-4xl font-sans font-bold my-[25px]">Contact</span>
+        <div className="h-[110vh] bg-violet-100 mb-20">
+        <div className="text-white mb-20">.</div>
+        <div className="flex items-center justify-center ">
+            <div className="text-center max-w-2xl">
+                <div className="mb-5">
+                    <span className="text-4xl font-sans font-bold">Get In Touch</span>    
+                </div>
+                <div className="flex justify-center mb-11">
+                    <hr className="w-24 h-1 bg-violet-400 border-0" />
+                </div>
+                <p className="text-lg font-gray-600">Feel free to reach out for collaborations or just a friendly chat.</p>
             </div>
+            
+        </div> 
+        <div className="flex gap-5 justify-center mt-10">
+            
         </div>
+        
+    </div>
     )
 }
--- src/comp/Skills.jsx
+++ src/comp/Skills.jsx
@@ -1,9 +1,83 @@
 export default function Skills(){
     return(
-        <div className="h-[70vh] bg-">
-            <div>
-                <span className="text-4xl font-sans font-bold my-[25px]">Skills</span>
+        <div className="h-[90vh] flex-col mb-20">
+            <div className="text-white mb-20">.</div>
+
+            <div className="flex items-center justify-center ">
+                <div className="text-center max-w-2xl">
+                    <div className="mb-5">
+                        <span className="text-4xl font-sans font-bold">Technical Skills</span>    
+                    </div>
+                    <div className="flex justify-center mb-7">
+                        <hr className="w-24 h-1 bg-violet-400 border-0" />
+                    </div>
+                   
+                    <p className="text-lg font-gray-600">Here's an overview of the technologies and tools I work with.</p>
+                    
+                </div>
+                
             </div>
-        </div>
+            <div className="text-left flex justify-center">
+
+            <div className="shadow-violet-500 mr-5 rounded-xl shadow-lg hover:shadow-2xl duration-200 ease-in w-100  py-10 px-5 mt-10 max-w-xl text-left bg-gradient-to-b from-white to-gray-100 hover:bg-gradient-to-t duration-400 ease-in">
+                <div className="font-semibold text-xl">Languages</div>
+                <ul class='list-outside list-disc ml-6 mt-5  text-md'>
+                    <li class="text-violet-500 mb-2">
+                        <span class="text-gray-700">TypeScript</span>
+                    </li>
+                    <li class="text-violet-500 mb-2">
+                        <span class="text-gray-700">JavaScript</span>
+                    </li>
+                    <li class="text-violet-500 mb-2">
+                        <span class="text-gray-700">HTML/CSS</span>
+                    </li>
+                    <li class="text-violet-500 mb-2">
+                        <span class="text-gray-700">SQL</span>
+                    </li>
+                    <li class="text-violet-500 mb-2">
+                        <span class="text-gray-700">C/C++</span>
+                    </li>
+                </ul>
+            </div>  
+
+            <div className="shadow-violet-500 mr-5 rounded-xl shadow-lg hover:shadow-2xl duration-200 ease-in w-100  py-10 px-5 mt-10 max-w-xl text-left bg-gradient-to-b from-white to-gray-100 hover:bg-gradient-to-t duration-400 ease-in">
+                <div className="font-semibold text-xl">Frontend</div>
+                <ul class='list-outside list-disc ml-6 mt-5  text-md'>
+                    <li class="text-violet-500 mb-2">
+                        <span class="text-gray-700">React.js</span>
+                    </li>
+                    <li class="text-violet-500 mb-2">
+                        <span class="text-gray-700">Tailwind CSS</span>
+                    </li>
+                    <li class="text-violet-500 mb-2">
+                        <span class="text-gray-700">Vite</span>
+                    </li>
+                    <li class="text-violet-500 mb-2">
+                        <span class="text-gray-700">ThreeJS</span>
+                    </li>
+                    
+                </ul>
+            </div>  
+
+            <div className="shadow-violet-500 mr-5 rounded-xl shadow-lg hover:shadow-2xl duration-200 ease-in w-100  py-10 px-5 mt-10 max-w-xl text-left bg-gradient-to-b from-white to-gray-100 hover:bg-gradient-to-t duration-400 ease-in">
+                <div className="font-semibold text-xl">Backend</div>
+                <ul class='list-outside list-disc ml-6 mt-5  text-md'>
+                    <li class="text-violet-500 mb-2">
+                        <span class="text-gray-700">Node.js</span>
+                    </li>
+                    <li class="text-violet-500 mb-2">
+                        <span class="text-gray-700">Express.js</span>
+                    </li>
+                    <li class="text-violet-500 mb-2">
+                        <span class="text-gray-700">WebSockets</span>
+                    </li>
+                    <li class="text-violet-500 mb-2">
+                        <span class="text-gray-700">REST APIs</span>
+                    </li>
+                    
+                </ul>
+            </div>                                  
+        </div>      
+    </div>
     )
 }
